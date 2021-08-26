50c1a41ed1da695f65088e64fb7abf746f300312
Adrian <lildemondsuxx@gmail.com>
2021-08-25 23:24:51 -0400

    update: added a structure text file to keep track on my plans for this project

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+void main();
+
 function main() {
     const canvas = document.querySelector("#glCanvas");
     // Initialize the GL context
@@ -7,8 +9,21 @@ function main() {
     if (gl === null) {
       alert("Unable to initialize WebGL. Your browser or machine may not support it.");
       return;
+    };
+
+     // Vertex shader program
+
+    const vsSource = `
+    attribute vec4 aVertexPosition;
+
+    uniform mat4 uModelViewMatrix;
+    uniform mat4 uProjectionMatrix;
+
+    void main() {
+      gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
     }
-  
+  `;
+
     // Set clear color to black, fully opaque
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     // Clear the color buffer with specified clear color
